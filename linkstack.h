c8e9af4060d91a76dd597a8d22b69e6e8c712b72
raiden <huangtao@jinke-pc.(none)>
2011-10-25 17:03:05 +0800

    linkstack defined

--- linkstack.h
+++ linkstack.h
@@ -1,4 +1,37 @@
 #ifndef _LINKSTACK_H
 #define _LINKSTACK_H
 
+#include "linkstack_define.h"
+
+struct linkstack_node {
+	StackElement data;
+	struct linkstack_node *next;
+};
+
+struct LinkStack {
+	Int32 length;
+	Int32 max_length;
+	struct linkstack_node *top;
+	struct linkstack_node *bottom;
+};
+
+struct linkstack_operations {
+	struct LinkStack* (*create) (void);
+	void (*destory) (struct LinkStack** stack);
+	Int32 (*clear) (struct LinkStack** stack);
+	struct linkstack_node* (*create_node) (void);
+
+	Boolean (*is_empty) (struct LinkStack *stack);
+	Boolean (*is_full) (struct LinkStack *stack);
+	Int32 (*length) (struct LinkStack *stack);
+	Int32 (*max_length) (struct LinkStack *stack);
+	linkstack_node* (*top) (struct LinkStack *stack);
+	linkstack_node* (*bottom) (struct LinkStack *stack);
+
+	Boolean (*push) (struct LinkStack *stack, StackElement elem);
+	Boolean (*push_node) (struct LinkStack *stack, struct linkstack_node *node);
+	StackElement (*pop) (struct LinkStack *stack);
+	struct linkstack_node* (*pop) (struct LinkStack *stack);
+};
+
 #endif // _LINKSTACK_H
--- linkstack_define.h
+++ linkstack_define.h
@@ -1,4 +1,8 @@
 #ifndef _LINKSTACK_DEFINE_H
 #define _LINKSTACK_DEFINE_H
 
+#include "cross_define.h"
+
+typedef Int32 StackElement;
+
 #endif // _LINKSTACK_DEFINE_H
